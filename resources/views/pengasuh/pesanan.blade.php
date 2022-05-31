1c1b4c0f7b083d3c2cfea1dbc4c4cb61da6e9237
rafmul3 <mrafimuli35@gmail.com>
2022-05-31 12:46:17 +0700

    final sprint2

--- resources/views/pengasuh/pesanan.blade.php
+++ resources/views/pengasuh/pesanan.blade.php
@@ -184,12 +184,12 @@
                                             {{-- modal konfirmasi selesai --}}
 
                                         <!-- Button trigger modal hapus-->
-                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#batal{{ $datas->id }}">
                                                 Tolak
                                             </button>
 
                                             <!-- Modal -->
-                                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal fade" id="batal{{ $datas->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
@@ -197,7 +197,7 @@
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                    Apakah Anda Yakin Ingin Menolak Pesanan?
+                                                    Apakah Anda Yakin Ingin Menolak Pesanan? {{ $datas->id }}
                                                     </div>
                                                     <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
