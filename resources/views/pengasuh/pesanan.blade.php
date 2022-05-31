dfd03a1651ef0d19904153f2b155a204c92f6d30
rafmul3 <mrafimuli35@gmail.com>
2022-05-31 12:49:26 +0700

    final sprint2

--- resources/views/pengasuh/pesanan.blade.php
+++ resources/views/pengasuh/pesanan.blade.php
@@ -197,7 +197,7 @@
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                    Apakah Anda Yakin Ingin Menolak Pesanan? {{ $datas->id }}
+                                                    Apakah Anda Yakin Ingin Menolak Pesanan?
                                                     </div>
                                                     <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
