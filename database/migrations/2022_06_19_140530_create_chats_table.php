535388ecec76ed97dad345f4fbfe1329c23a030f
insouc <alhafizh_arfin@yahoo.com>
2022-06-21 10:52:40 +0700

    bug

--- database/migrations/2022_06_19_140530_create_chats_table.php
+++ database/migrations/2022_06_19_140530_create_chats_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
             $table->foreignId('room_id')->references('id')->on('rooms')->onDelete('cascade');
-            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('message');
             $table->enum('status', ['terkirim', 'read']);
             $table->timestamps();
